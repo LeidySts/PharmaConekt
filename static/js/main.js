cbc990d9b7f76d00ade20e678feebcfae9653a10
Leidiane santos <leidiane23si@gmail.com>
2026-06-23 18:31:51 -0300

    Migrando o PharmaConekt para a raiz do projeto e mantendo o planejamento

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,6 @@
-// Confirmações exclusão de registros
-document.addEventListener('DOMContentLoaded', function() {
-    const deleteButtons = document.querySelectorAll('.btn-confirm-delete');
-    deleteButtons.forEach(button => {
-        button.addEventListener('click', function(e) {
-            if(!confirm("Tem certeza que deseja remover esta farmácia do fluxo unificado de dados?")) {
-                e.preventDefault();
-            }
-        });
-    });
-});
+body{
+  padding: 25px;
+}
+.title {
+	color: #5C6AC4;
+}
